83144713868a4a38c4312c4c3d34d9aa7bbd4017
Krzysztof Olejniczak <krzole002@utp.edu.pl>
2021-01-07 21:10:21 +0100

    DrzewoFirst

---
+++ tree.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+ unsigned gwiazdka, wysokosc;
+ int main ()
+ {
+     printf("Podaj wysokosc choink:");
+     scanf("%d", &wysokosc);
+
+     for (gwiazdka = 0; gwiazdka < wysokosc; gwiazdka++)
+     {
+
+     }
+
+         
+         
+     return 0; // asdasasd
+     
+ }
+
